48dbb1148d3aa0d03584f390e16d8e4ad264eb8a
Oktavian Yoga <70311310+oktoala@users.noreply.github.com>
2023-01-25 23:17:36 +0800

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<section class="w-full">
+	<div class="container py-6 flex items-center justify-between">
+		<h1
+			class="text-5xl font-bold bg-gradient-to-r bg-clip-text from-primary via-secondary to-third text-transparent"
+		>
+			Opps... Something Wrong!
+		</h1>
+	</div>
+</section>
